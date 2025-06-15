3d9e8cb1ad73e757e5f7d8cfb1286cd0c0b1bdb1
Aitzaz Hassan <aitzazhakro123@gmail.com>
2025-06-15 22:42:30 +0500

    .

---
+++ script.js
@@ -0,0 +1,10 @@
+var xmark= document.querySelector(".xmark");
+var sidebarr= document.querySelector(".sidebar");
+
+
+function ShowsideBar(){
+
+   sidebarr.style.visibility = "hidden";
+   sidebarr.style.backgroundcolor = "yellow ";
+}
+
