4f8c901a399fa94fb2d2c0135398e31b7f9312af
Aitzaz Hassan <aitzazhakro123@gmail.com>
2025-06-16 15:34:55 +0500

    added some sidebar functionality

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 var xmark= document.querySelector(".xmark");
 var sidebarr= document.querySelector(".sidebar");
 
+xmark.addEventListener("click",ShowsideBar());
 
 function ShowsideBar(){
 
    sidebarr.style.visibility = "hidden";
    sidebarr.style.backgroundcolor = "yellow ";
 }
-
